039a2073ba3beb0e6d7117070029c849c2ae8228
Atsumori-JAIST <s2010401@jaist.ac.jp>
2021-05-29 21:56:04 +0900

    Get message test

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ function handleEvent(event) {
 
   // create a echoing text message
   const echo = { type: 'text', text: event.message.text };
+  console.log('TESTETST!!!!');
 
   // use reply API
   return client.replyMessage(event.replyToken, echo);
