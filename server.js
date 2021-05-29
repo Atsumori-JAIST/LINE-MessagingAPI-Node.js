f05c5c55d1a404e555ab6557bac4326d421049c1
Atsumori-JAIST <s2010401@jaist.ac.jp>
2021-05-30 07:10:22 +0900

    05/30-7:10

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ function handleEvent(event) {
 
   // create a echoing text message
   //const echo = { type: 'text', text: event.message.text };
-  const testmsg = {type: 'text', text: event}
+  const testmsg = {type: 'text', text: event.source.userId}
 
   // use reply API
   //return client.replyMessage(event.replyToken, echo);
